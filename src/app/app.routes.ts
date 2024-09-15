3c3cb73d75d4f699dfba842670835813a33c2360
Kyler Haag <kylerhaag@Kylers-MBP.attlocal.net>
2024-09-14 19:36:48 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { BudgetsComponent } from './budgets/budgets.component'; 
+import { GoalsComponent } from './goals/goals.component'; 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'accounts', component: AccountsComponent },
+    { path: 'transactions', component: TransactionsComponent },
+    { path: 'budgets', component: BudgetsComponent },
+    { path: 'goals', component: GoalsComponent },
+    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  ];
+  
+// export const routes: Routes = [];
